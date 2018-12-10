7e58cf1ff6559ac44d68a073d9e0b2470b10f0c8
Jason.Eu <morty.yu@yahoo.com>
2018-12-11 00:23:24 +0800

    reasonable spglib ir test

--- test/test_spglibwrapper.jl
+++ test/test_spglibwrapper.jl
@@ -2,11 +2,11 @@ include("../src/spglibwrapper.jl")
 
 using Test
 
-latt = Array{Float64, 2}([0 0.5 0.5; 0.5 0.0 0.5; 0.5 0.5 0])*5.4
-positions = Array{Float64, 2}([0.875 0.875 0.875; 0.125 0.125 0.125])
-atoms = Vector{Int}([1; 1])
+latt = Array{Float64, 2}([0.5 0.0 14.0; 0.0 0.5 14.0; 0.0 0.0 14.0])*5.4
+positions = Array{Float64, 2}([0.0 0.0 0.0])
+atoms = Vector{Int}([1])
 cell = Cell(latt, positions, atoms)
 @testset "spglibwrapper testset" begin
-    Nirk, grid, mapping = get_ir_reciprocal_mesh([8;8;8], cell, [0;0;0])
-    @test Nirk == 59
+    Nirk, = get_ir_reciprocal_mesh([2;2;1], cell, [0;0;0])
+    @test Nirk == 3
 end
