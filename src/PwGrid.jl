b59ce1817696ab0ac2a5b0aaeea47ca0038a2086
Jason.Yu <morty.yu@yahoo.com>
2018-12-13 15:17:07 +0800

    init_Gvectors PwGrid.jl

--- src/PwGrid.jl
+++ src/PwGrid.jl
@@ -7,8 +7,8 @@ The type for set of G-vectors for describing density and potentials
 struct GVectors
     Ng::Int64
     G::Array{Float64, 2}
-    G2::Array{Float64, 1}
-    idx_g2r::Array{Int64, 1}
+    G_length::Vector{Float64}
+    # idx_g2r::Array{Int64, 1}
 end # GVectors struct
 
 """
@@ -29,8 +29,8 @@ struct PWGrid
     ecutwfc::Float64    # energy cut of wavefunction
     ecutrho::Float64    #
     Ns::Tuple{Int64, Int64, Int64}
-    LatVecs::Array{Float64, 2}
-    RecVecs::Array{Float64, 2}
+    lattice::Array{Float64, 2}
+    recLatt::Array{Float64, 2}
     CellVolume::Float64
     r::Array{Float64, 2}
     gvec::GVectors
@@ -39,55 +39,83 @@ struct PWGrid
     planbw  # return by FFTW.plan_ifft()
 end # PWGrid struct
 
-function PWGrid(ecutwfc::Float64, latt::Array{Float64, 2}; kpoints=nothing)
+function PWGrid(ecutwfc::Float64, lattice::Array{Float64, 2}; kpoints=nothing)
 
     ecutrho = 4.0*ecutwfc
-    RecVecs = 2π * latt'
+    recLatt = 2π * inv(transpose(lattice))
 
-    latLen = Vector{Float64}(undef, 3)
+    lattLen = Vector{Float64}(undef, 3)
     for i in 1:3
-        latLen[i] = norm(latt[:, i])
+        lattLen[i] = norm(lattice[i, :])
     end
 
-    ns = map(x -> 2*round(Int64, √(ecutrho/2)x/π) + 1, latLen)
+    Ns = map(x -> 2*round(Int64, √(ecutrho/2)x/π) + 1, lattLen)
 
-    r = init_grid_R(latt, ns)
+    r = init_grid_real(lattice, Ns)
 
-    # gvec = init_gvec(ns, RecVecs, ecutrho)
-    #
-    # if kpoints == nothing
-    #     kpoints = KPoints(1, (1,1,1), zeros(1,3), [1.0], RecVecs)
-    # end
-    #
-    # gvecw = init_gvecw(ecutwfc, gve, kpoints)
-    #
-    # planfw = plan_fft(zeros(ns))
-    # planbw = plan_ifft(zeros(ns))
+    gvec = init_Gvectors(ns, recLatt, ecutrho)
 
-    return PWGrid(ecutwfc, ecutrho, ns, latt, RecVecs, CellVolume, r, gvec, gvecw,
+    if kpoints == nothing
+        kpoints = KPoints(1, (1,1,1), zeros(1,3), [1.0], recLattice)
+    end
+
+    gvecw = init_gvecw(ecutwfc, gvec, kpoints)
+
+    planfw = plan_fft(zeros(ns))
+    planbw = plan_ifft(zeros(ns))
+
+    return PWGrid(ecutwfc, ecutrho, (Ns[1], Ns[2], Ns[3]),
+                  lattice, recLatt, CellVolume, r,
+                  gvec, gvecw,
                   planfw, planbw)
 
 end # PwGrid module
 
-function grid_num(len_latt, ecutrho)
-    n = 2*round(Int64, sqrt(ecutwfc/2)*len_latt/π) + 1
-    # n = good_fft_order(n)
-    return n
-end
-
 """
 实空间中的点阵坐标
 """
-function init_grid_R(latt, ns)
-    R = Array{Float64, 2}(undef, prod(ns), 3)
+function init_grid_real(lattice, Ns)
+    r = Array{Float64, 2}(undef, prod(Ns), 3)
     # b is the smallest grid unit
-    b = latt .* (1 ./ hcat(ns, ns, ns))
-    idx = 1
-    for i in Iterators.product(0:ns[1]-1, 0:ns[2]-1, 0:ns[3]-1)
+    b = lattice .* (1 ./ hcat(Ns, Ns, Ns))
+    iter_points = Iterators.product(0:Ns[1]-1, 0:Ns[2]-1, 0:Ns[3]-1)
+    for (idx, i) in enumerate(iter_points)
         scale = collect(i)
-        R[idx, :] = scale' * b
-        idx += 1
+        r[idx, :] = transpose(scale) * b
     end
 
-    return R
-end # init_grid_R function
+    return r
+end # init_grid_real function
+
+"""
+倒空间中满足|g|^2 < 2ecutrho的点, 及其到Γ的距离
+"""
+function init_Gvectors(recLatt::Array{Float64, 2}, Ns::Vector{Int64}, ecutrho::Float64)
+    iter_points = Iterators.product(
+        ceil(Int64, -Ns[1]/2):ceil(Int64, Ns[1]/2)-1,
+        ceil(Int64, -Ns[2]/2):ceil(Int64, Ns[2]/2)-1,
+        ceil(Int64, -Ns[3]/2):ceil(Int64, Ns[3]/2)-1
+    )
+
+    G_tmp = []
+    G_length = []
+    for (idx, i) in enumerate(iter_points)
+        scale = collect(i)
+        g = transpose(scale) * recLatt
+        gl = norm(g)
+        if gl < √(2ecutrho)
+            push!(G_tmp, g)
+            push!(G_length, gl)
+        end
+    end
+
+    Ng = length(G_tmp)
+    G = Array{Float64, 2}(undef, Ng, 3)
+    for i = 1:Ng
+        G[i,:] = transpose(G_tmp[i])
+    end
+
+    G_length = convert(Vector{Float64}, G_length)
+
+    return GVectors(Ng, G, G_length)
+end
--- test/test_PwGrid.jl
+++ test/test_PwGrid.jl
@@ -1,12 +1,13 @@
 include("../src/PwGrid.jl")
 
 using Test
+using LinearAlgebra
 
 @testset "init_grid_R test" begin
     latt = Array{Float64, 2}([10 10 10; 5 0 5; 5 5 0])
     ns = Vector{Int64}([10, 8, 6])
-    r = init_grid_R(latt, ns)
-    
+    r = init_grid_real(latt, ns)
+
     function has_row(v, m)
         return any(Bool[v == m[i, :]' for i=1:size(m, 1)])
     end
@@ -14,4 +15,19 @@ using Test
     @test size(r) == (480, 3)
     @test has_row([0 0 0], r)
     @test has_row([3 3 3], r)
+end
+
+@testset "init_Gvectors testset" begin
+    latt = [5.0 0 0; 0 5.0 0; 0 0 5.0]
+
+    recLatt = 2π * inv(transpose(latt))
+    lattLen = Vector{Float64}(undef, 3)
+    for i in 1:3
+        lattLen[i] = norm(latt[i, :])
+    end
+    ecutrho = 120.0
+    Ns = map(x -> 2*round(Int64, √(ecutrho/2)x/π) + 1, lattLen)
+
+    gvec = init_Gvectors(recLatt, Ns, ecutrho)
+    @test size(gvec.G, 1) == size(gvec.G_length, 1) == gvec.Ng
 end
